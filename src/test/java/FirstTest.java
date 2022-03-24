a95ef2f5aaa5a02bcbdf1ac763016c7c439e6b60
Rossita <47862499+AaronsonRossita@users.noreply.github.com>
2022-03-24 21:11:49 +0200

    Changes

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -8,17 +8,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class FirstTest {
 
     @BeforeClass
     public static void setupClass(){
         WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
     }
 
     @Test
     public void openBrowser() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.canva.com/");
+        driver.get(Helper.CANVA);
         Thread.sleep(5000);
         driver.quit();
     }
@@ -26,11 +29,75 @@ public class FirstTest {
     @Test
     public void clickLogin() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
+        driver.get(Helper.SAUCEDEMO);
         Thread.sleep(5000);
-        WebDriverWait wait = new WebDriverWait(driver,5);
-        WebElement loginBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("login-button")));
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+//        By.id()
+        WebElement loginBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.LOGINBTN)));
+//        WebElement loginBtn = driver.findElement(By.id("login-button"));
         loginBtn.click();
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+
+
+    @Test
+    public void findElement() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        Duration duration = Duration.ofSeconds(5);
+        driver.get(Helper.CANVA);
+        WebDriverWait wait = new WebDriverWait(driver,duration);
+//               By.xpath()
+//        WebElement loginBtn = wait
+//                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(Helper.CANVALOGINXPATH)));
+//        By.cssSelector()
+        WebElement loginBtn = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(Helper.CANVACSSSELECTOR)));
+        Thread.sleep(3000);
+        loginBtn.click();
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+    @Test
+    public void loginUser() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get(Helper.SAUCEDEMO);
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+        WebElement loginField = wait
+                .until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.LOGINID)));
+        WebElement passwordField = wait
+                .until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.PASSWORDID)));
+        WebElement loginBtn = wait
+                .until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.LOGINBTN)));
+        loginField.sendKeys(Helper.USERNAME);
+        Thread.sleep(3000);
+        passwordField.sendKeys(Helper.PASSWORD);
+        Thread.sleep(3000);
+        loginBtn.click();
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+    @Test
+    public void loginUsers() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        for (int i = 0; i < Helper.USERNAMEARR.length; i++) {
+            driver.get(Helper.SAUCEDEMO);
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+            WebElement loginField = wait
+                    .until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.LOGINID)));
+            WebElement passwordField = wait
+                    .until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.PASSWORDID)));
+            WebElement loginBtn = wait
+                    .until(ExpectedConditions.presenceOfElementLocated(By.id(Helper.LOGINBTN)));
+            loginField.sendKeys(Helper.USERNAMEARR[i]);
+            Thread.sleep(3000);
+            passwordField.sendKeys(Helper.PASSWORD);
+            Thread.sleep(3000);
+            loginBtn.click();
+            Thread.sleep(3000);
+        }
     }
 
 
---
+++ src/test/java/Helper.java
@@ -0,0 +1,18 @@
+public class Helper {
+
+    static final String SAUCEDEMO = "https://www.saucedemo.com/";
+    static final String SAUCELOGINBTNID = "login-button";
+    static final String LOGINID = "user-name";
+    static final String PASSWORDID = "password";
+    static final String LOGINBTN = "login-button";
+    static final String USERNAME = "standard_user";
+    static final String[] USERNAMEARR = {"standard_user","locked_out_user","problem_user","performance_glitch_user"};
+    static final String PASSWORD = "secret_sauce";
+
+
+    static final String CANVA = "https://www.canva.com/";
+    static final String CANVALOGINXPATH = "//*[@id=\"root\"]/div/div[3]/div/div[2]/div/header/div[6]/button[1]";
+    static final String CANVACSSSELECTOR = "#root > div > div.M6yXdA.hkW4sQ > div > div.IMy50w.a6f7yQ > div > header > div.PYoGFg.DwB3TQ > button._1QoxDw.Qkd66A.tYI0Vw.o4TrkA.NT2yCg.Qkd66A.tYI0Vw.lsXp-w.cwOZMg.zQlusQ.uRvRjQ.IJUswg._08uH8A";
+
+
+}
